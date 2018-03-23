60bc89edeb4709247d5aed3f6e75992d1d3bbde0
rain1 <rain1@airmail.cc>
2018-03-23 22:32:59 +0000

    mal tests

---
+++ t/mal/inc.scm
@@ -0,0 +1,18 @@
+(define (inc1 a) (+ 1 a))
+(define (inc2 a) (+ 2 a))
+(define (inc3 a)
+  (+ 3 a))
+
+(define (inc4 a) ;; a comment after code
+  (+ 4 a))
+
+(inc1 5)
+(inc2 5)
+(inc3 5)
+(inc4 5)
+
+(inc1 6)
+(inc2 6)
+(inc3 6)
+(inc4 6)
+
