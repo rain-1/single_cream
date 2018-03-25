cebc03e8c186a37d50510585866e25f9613b55eb
rain1 <rain1@airmail.cc>
2018-03-25 20:18:55 +0000

    remove punctuation

--- t/simple/reverse-1.scm
+++ t/simple/reverse-1.scm
@@ -2,8 +2,8 @@
 (define q
   '(the scorn for riches displayed by the philosophers was a secret desire
     to recompense their own merit for the injustice of Fortune by scorning
-    those very benefits she had denied them; it was a private way of
-    remaining unsullied by poverty, a devious path towards the high
+    those very benefits she had denied them it was a private way of
+    remaining unsullied by poverty a devious path towards the high
     respect they could not command by wealth))
 (reverse q)
 (reverse (reverse q))
