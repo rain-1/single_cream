79fe2a88f3ade4c34d78f89337518ffee64889c3
rain1 <rain1@airmail.cc>
2018-09-24 14:29:03 +0100

    sort by function

--- src/init.scm
+++ src/init.scm
@@ -111,3 +111,19 @@
       (if (equal? key (caar table))
 	  (car table)
 	  (assoc key (cdr table)))))
+
+(define (comparing < f)
+  (lambda (x y)
+    (< (f x) (f y))))
+
+(define (insert-by < elt rest)
+  (if (null? rest)
+      (cons elt '())
+      (if (< elt (car rest))
+          (cons elt rest)
+          (cons (car rest) (insert-by < elt (cdr rest))))))
+
+(define (sort-by < list)
+  (if (null? list)
+      list
+      (insert-by < (car list) (sort-by < (cdr list)))))
