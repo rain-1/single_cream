3a2087c5d3e1d6dad31fd2fcaff04d4d47a51aad
rain1 <rain1@airmail.cc>
2018-12-23 00:17:52 +0000

    add rosettacode balanced brackets example

---
+++ t/rosetta/balancedbrackets.scm
@@ -0,0 +1,30 @@
+(define (balanced-brackets string)
+  (print (b string 0)))
+
+(define (boolean-and b1 b2)
+  (if b1
+      b2
+      #f))
+
+(define (b chars sum)
+  (if (boolean-and (null? chars) (= 0 sum))
+      #t
+      (if (null? chars)
+          #f
+          (if (char=? #\[ (car chars))
+              (b (cdr chars) (+ sum 1))
+              (if (= sum 0)
+		  #f
+		  (b (cdr chars) (- sum 1)))))))
+
+(begin
+  (balanced-brackets "")
+  
+  (balanced-brackets "[]")
+  (balanced-brackets "[][]")
+  (balanced-brackets "[[][]]")
+  
+  (balanced-brackets "][")
+  (balanced-brackets "][][")
+  (balanced-brackets "[]][[]"))
+
